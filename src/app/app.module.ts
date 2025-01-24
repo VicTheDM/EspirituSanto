0f2a43ca7c09bd525873c2d5658d418949242503
VicTheDM <victorsandovalsoftwaredev@gmail.com>
2025-01-24 12:29:58 -0600

    bluehalo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
 
 @NgModule({
   imports: [
